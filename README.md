ed2cdf281d15538b1a4812f69e0afccc8f78e244
David Stewart <dave@metanurb.com>
2019-04-21 17:19:20 -0400

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# CryptoBot
-Discord 3 in 1 Crypto Bot.
+[TOC]
+
+#### CryptoBots
+
+Discord 3 (so far) in 1 Crypto Bot.
+
+**Gann Square 9 generator bot for BTC.**
+
+- Discord usage: !GS9 `<last traded price>`
+
+**Bitmex current largest Buy and Sell wall.**
+
+- Discord usage: !bitbook
+
+**Options implied Volatility.**
+
+- Discord usage:
+  Expected Cost - The market price of the option
+  s - Current price of the underlying
+  k - Strike price
+  t - Time to experiation in years (can be decimal)
+  r - Anual risk-free interest rate as a decimal
+  CallPut - The type of option priced - "call" or "put"
+  Input as a list delimted with spaces
+  Example: !oiv 2 101 100 0.1 0.0015 call
+  To verify your result using Black-Scholes Model
+  Example: !oiv 101 100 0.1 YOUR_IV_RESULT 0.0015 call bs
+
+#### Heroku Installation
+
+Download or clone this repo.
+In a terminal, CD to this directory.
+
+Using the Heroku CLI, login and create a new application.
+`git init`
+`heroku git:remote -a your-app-name`
+`git add .`
+
+`git commit -am "make it better"`
+`git push heroku master`
+
+This source has to be run as a worker Dyno, not a web Dyno.
+The code in the provided Procfile should handle this automatically.
+
+#### Discord Installation
+
+In a web browser, navigate to https://discordapp.com/developers and login.
+Create a new application & follow the instructions to complete this.
+Don't change any of the settings, just create a **CLIENT ID** and copy it to a textfile for later use.
+
+From within this new applications menu, click Bots and create a new bot. Give it a name and icon.
+Don't change any of the settings, just create a **Token** and copy it to a textfile for later use.
+
+From your Heroku account page, go to the applications page, click Settings and from there, click **Reveal Config Vars**. Add a new var: `BOT_TOKEN`
+The value of this token should be the copied bot **token** we grabbed earlier.
+
+While still in settings, click **Add Buildpack** and add:
+https://github.com/jontewks/puppeteer-heroku-buildpack
+This is required because the GS9 bot creates HTML that needs screenshot to deliver an image to the discord channel. Pupeteer fires up an instance of Chromium to render this DOM element to a PNG file.
+That's it for Heroku.
+
+In that text file you created earlier, jot down this URL and place the copied **CLIENT ID** inside it, like so:
+https://discordapp.com/api/oauth2/authorize?client_id=PUT_CLIENT_ID_HERE&scope=bot&permissions=0
+
+Next, paste this url into the browser and you will be directed to a page that asks you to Authenticate this bot with your Discord server. Of course you must have Admin privelages on the channel to do this.
+If all goes well, your new bot should come on-line from within your channel.
+
+If it does not respond as expected, from your Heroku app dashboard page, view the logs and see if there was an error building or deploying or running your bot. Your on your own to figure out any errors from here though. Sometimes it goes smooth, others not so much.
+
+Good Luck.
---
+++ app.js
@@ -0,0 +1,502 @@
+const puppeteer = require("puppeteer");
+const discord = require("discord.js");
+const getJSON = require("get-json");
+const numeral = require("numeral");
+const iv = require("implied-volatility");
+const bs = require("black-scholes");
+
+const discord_client = new discord.Client();
+
+discord_client.on("ready", () => {
+  console.log("Connected as: " + discord_client.user.tag);
+});
+
+discord_client.on("message", receivedMessage => {
+  // Prevent bot from responding to its own messages
+  if (receivedMessage.author == discord_client.user) {
+    return;
+  }
+  if (receivedMessage.content.startsWith("!")) {
+    processCommand(receivedMessage);
+  }
+});
+
+function processCommand(receivedMessage) {
+  let fullCommand = receivedMessage.content.substr(1); // Remove the leading exclamation mark
+  let splitCommand = fullCommand.split(" "); // Split the message up in to pieces for each space
+  let botID = splitCommand[0]; // The first word directly after the exclamation is the command
+  let arguments = splitCommand.slice(1); // All other words are arguments/parameters/options for the command
+  let ltp;
+  let doHelp = false;
+  let doBs = false;
+  let help;
+
+  if (arguments === "help" || arguments.length === 0) {
+    if (botID === "GS9") {
+      help =
+        "Gann9 Bot by DaveStewart#0241 V1.0\n" +
+        "Lets say you have a LTP price at 5152.\n" +
+        "You enter the price in !GS9 5152\n" +
+        "You will get the square that looks like below \n" +
+        "5152 falls between 5166 and 5148.\n" +
+        "So we should buy above 5166 and sell below 5148.\n" +
+        "Targets are next levels in the calculator.\n" +
+        "Now lets say 1st target is achieved.(5181)\n" +
+        "Then we we watch whether price is able to sustain above 5181 for more than 5 min.\n" +
+        "If it sustains, we continue our long position. If not, we square off.\n\n" +
+        "But how to find the targets.?\n" +
+        "For Buy, we go in clockwise direction from the Yellow LTP box\n" +
+        "For Sell, we go in an anticlockwise direction.\n\n" +
+        "Clockwise targets are resistances for Buy. \n" +
+        "Anticlockwise are support for Sell.\n" +
+        "Targets are 99.95% of resistance and 100.05% of support.\n" +
+        "So Target for Buy = resistance * 0.9995. \n" +
+        "Target for Sell = support * 1.0005.";
+    } else if (botID === "bitbook") {
+      help = "?";
+    } else if (botID === "oiv") {
+      help =
+        "Determine implied volatility of options based on their prices\n" +
+        "Expected Cost - The market price of the option \n" +
+        "s - Current price of the underlying \n" +
+        "k - Strike price \n" +
+        "t - Time to experiation in years (can be decimal)\n" +
+        "r - Anual risk-free interest rate as a decimal \n" +
+        'CallPut - The type of option priced - "call" or "put" \n' +
+        "Input as a list delimted with spaces\n" +
+        "Example: !oiv 2 101 100 0.1 0.0015 call\n" +
+        "To verify your result using Black-Scholes Model\n" +
+        "Example: !oiv 101 100 0.1 YOUR_IV_RESULT 0.0015 call bs";
+    }
+    ltp = 5152;
+    doHelp = true;
+  } else {
+    if (botID === "oiv") {
+      // Black Scholes
+      if (arguments[arguments.length - 1] === "bs") {
+        arguments.pop();
+        var callput = arguments.pop().toString();
+        arguments = arguments.map(Number);
+        arguments.push(callput);
+        doBs = true;
+      } else {
+        doBs = false;
+        var callput = arguments.pop().toString();
+        //callput = callput.replace(/'/g, '"');
+        arguments = arguments.map(Number);
+        arguments.push(callput);
+      }
+    }
+    ltp = arguments;
+    doHelp = false;
+  }
+
+  //console.log("Command received: " + botID);
+  //console.log("Arguments: " + arguments); // There may not be any arguments
+
+  if (botID === "GS9") {
+    var squareAry = [
+      24,
+      23,
+      16,
+      17,
+      18,
+      25,
+      32,
+      31,
+      30,
+      29,
+      22,
+      15,
+      8,
+      9,
+      10,
+      11,
+      12,
+      19,
+      26,
+      33,
+      40,
+      39,
+      38,
+      37,
+      36,
+      35,
+      28,
+      21,
+      14,
+      7,
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      13,
+      20,
+      27,
+      34,
+      41,
+      48,
+      47,
+      46,
+      45,
+      44,
+      43,
+      42
+    ];
+    var idxArray = [
+      23,
+      16,
+      17,
+      18,
+      25,
+      32,
+      31,
+      30,
+      22,
+      8,
+      10,
+      12,
+      26,
+      40,
+      38,
+      36,
+      21,
+      0,
+      3,
+      6,
+      27,
+      48,
+      45,
+      42
+    ];
+    var green = [0, 6, 8, 12, 16, 18, 30, 32, 36, 40, 42, 48];
+
+    let next = 0;
+    let sqt = Math.sqrt(ltp);
+    //console.log("lpt sqt: ", sqt);
+    let flr = Math.floor(sqt);
+    //console.log("flr: ", flr);
+    let down = flr - 1;
+    //console.log("down: ", down);
+    let ary = new Array();
+    let ary2 = new Array();
+    let center = Math.pow(down, 2);
+    ary.push({ idx: 24, value: center });
+    //console.log("center: ", center);
+    next = down + 0.125;
+    let rows = 7;
+
+    for (let i = 0; i < idxArray.length; i++) {
+      let nextSqr = Math.pow(next, 2);
+      nextSqr = Math.floor(nextSqr * 100) / 100;
+      ary.push({ idx: idxArray[i], value: nextSqr });
+      next += 0.125;
+    }
+
+    var stop = false;
+    var lptId = 0;
+
+    var recommend = [
+      {
+        above: 0,
+        targets: [],
+        stop: 0
+      },
+      {
+        below: 0,
+        targets: [],
+        stop: 0
+      }
+    ];
+
+    var t1, t2, t3, t4;
+    for (i = 0; i < ary.length - 1; i++) {
+      if (ltp > ary[i].value && ltp < ary[i + 1].value && !stop) {
+        lptId = ary[i].idx;
+        //console.log("lptId: ", lptId);
+        recommend[0].above = ary[i + 1].value;
+        t1 = parseFloat((ary[i + 2].value * 0.9995).toFixed(2));
+        t2 = parseFloat((ary[i + 3].value * 0.9995).toFixed(2));
+        t3 = parseFloat((ary[i + 4].value * 0.9995).toFixed(2));
+        t4 = parseFloat((ary[i + 5].value * 0.9995).toFixed(2));
+        recommend[0].targets.push(t1, t2, t3, t4);
+        recommend[0].stop = ary[i].value;
+
+        recommend[1].below = ary[i].value;
+        t1 = parseFloat((ary[i - 1].value * 1.0005).toFixed(2));
+        t2 = parseFloat((ary[i - 2].value * 1.0005).toFixed(2));
+        t3 = parseFloat((ary[i - 3].value * 1.0005).toFixed(2));
+        t4 = parseFloat((ary[i - 4].value * 1.0005).toFixed(2));
+        recommend[1].targets.push(t1, t2, t3, t4);
+        recommend[1].stop = ary[i + 1].value;
+        stop = true;
+      }
+    }
+
+    var idx1;
+    for (let i = 0; i < squareAry.length; i++) {
+      if (squareAry[i] === lptId) {
+        idx1 = squareAry[i + 1];
+      }
+      for (let j = 0; j < ary.length; j++) {
+        if (ary[j].idx === squareAry[i]) {
+          var id = squareAry[i];
+          squareAry[i] = { idx: id, value: ary[j].value };
+        }
+      }
+    }
+    for (let i = 0; i < squareAry.length; i++) {
+      if (squareAry[i] === idx1) {
+        squareAry[i] = { idx: idx1, value: parseFloat(ltp) };
+      }
+      if (!isNaN(squareAry[i])) {
+        var id = squareAry[i];
+        squareAry[i] = { idx: id, value: -1 };
+      }
+    }
+    squareAry.sort((a, b) => parseFloat(a.idx) - parseFloat(b.idx));
+    //console.log(squareAry);
+
+    let htmlString =
+      "<style>" +
+      "table {" +
+      "width: 100%;" +
+      "height: 100%;" +
+      "background-color: rgb(67.9%, 28.6%, 28.6%);" +
+      "}" +
+      "table," +
+      "td {" +
+      "border: 1px solid rgb(36.2%, 3.5%, 3.5%);" +
+      "border-collapse: collapse;" +
+      "padding: 5px;" +
+      "text-align: center;" +
+      "background-color: rgb(67.9%, 28.6%, 28.6%);" +
+      "color: white;" +
+      "font-weight: bold;" +
+      "font-family: Arial, Helvetica, sans-serif;" +
+      "}" +
+      "table," +
+      "td {" +
+      "}" +
+      "</style>" +
+      "<table id='results_table'><tbody>";
+
+    var count = 0;
+    for (let j = 0; j < 7; j++) {
+      htmlString += "<tr>";
+      for (let k = 0; k < 7; k++) {
+        if (squareAry[count].value === -1) {
+          htmlString +=
+            "<td style='background-color:rgb(66.7%, 10.6%, 10.6%);color:white;'></td>";
+        } else {
+          if (squareAry[count].idx === idx1) {
+            htmlString +=
+              "<td style='background-color:rgb(88.6%, 78%, 21%);color:black;line-height: 13px;'>" +
+              squareAry[count].value +
+              "</br>ltp</td>";
+          } else {
+            if (squareAry[count].idx === 24) {
+              htmlString +=
+                "<td style='background-color:rgb(67.9%, 28.6%, 28.6%);color:white;'>" +
+                squareAry[count].value +
+                "</td>";
+            } else {
+              var iddx = squareAry[count].idx;
+              var target = green.includes(iddx);
+              var color = target
+                ? "rgb(18.9%, 48%, 18.9%)"
+                : "rgb(54%, 7.4%, 83.5%)";
+              htmlString +=
+                "<td style='background-color:" +
+                color +
+                ";color:white;'>" +
+                squareAry[count].value +
+                "</td>";
+            }
+          }
+        }
+        count++;
+      }
+      htmlString += "</tr>";
+    }
+    htmlString += "</td></tr></tbody></table>";
+
+    //console.log(html);
+    if (doHelp) {
+      receivedMessage.channel.send(help);
+    }
+
+    var recommendString =
+      "Bot by DaveStewart#0241 V1.0\n" +
+      "Recommendations:\n" +
+      "Buy at / above: " +
+      recommend[0].above +
+      "\nTargets: " +
+      recommend[0].targets[0] +
+      " - " +
+      recommend[0].targets[1] +
+      " - " +
+      recommend[0].targets[2] +
+      " - " +
+      recommend[0].targets[3] +
+      "\nStoploss : " +
+      recommend[0].stop +
+      "\nSell at / below: " +
+      recommend[1].below +
+      "\nTargets: " +
+      recommend[1].targets[0] +
+      " - " +
+      recommend[1].targets[1] +
+      " - " +
+      recommend[1].targets[2] +
+      " - " +
+      recommend[1].targets[3] +
+      "\nStoploss : " +
+      recommend[1].stop;
+
+    (async () => {
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+      });
+      const page = await browser.newPage();
+      await page.setContent(htmlString);
+      //await page.screenshot({ path: "GS9.png" });
+      await page.screenshot({ path: "GS9.png" }).then(function(buffer) {
+        const localFileAttachment = new discord.Attachment("GS9.png");
+        receivedMessage.channel.send(localFileAttachment);
+        receivedMessage.channel.send(recommendString);
+      });
+      await browser.close();
+    })();
+  } else if (botID === "bitbook") {
+    getJSON("https://www.bitmex.com/api/v1/orderBook/L2?symbol=XBTUSD&depth=0")
+      .then(function(response) {
+        response.sort(
+          sort_by("side", {
+            name: "size",
+            primer: parseInt,
+            reverse: true
+          })
+        );
+
+        var resp =
+          "Bot by DaveStewart#0241 V1.0\nBitmex XBTUSD Order Book as of " +
+          new Date() +
+          "\n";
+        //var sell = false;
+        //var price;
+
+        resp +=
+          "Largest Buy Wall: " +
+          numeral(response[0].size).format("( 0.0a)") +
+          " @: " +
+          numeral(response[0].price).format("($0.00a)");
+
+        const sIndex = response.findIndex(item => item.side === "Sell");
+
+        resp +=
+          "\nLargest Sell Wall: " +
+          numeral(response[sIndex].size).format("( 0.0a)") +
+          " @: " +
+          numeral(response[sIndex].price).format("($0.00a)");
+
+        receivedMessage.channel.send(resp);
+      })
+      .catch(function(error) {
+        console.log(error);
+        //receivedMessage.channel.send(error);
+      });
+  } else if (botID === "oiv") {
+    if (doHelp) {
+      receivedMessage.channel.send(help);
+    } else {
+      if (doBs) {
+        oiv_value = bs.blackScholes(
+          arguments[0],
+          arguments[1],
+          arguments[2],
+          arguments[3],
+          arguments[4],
+          arguments[5]
+        );
+        receivedMessage.channel.send("Black-Scholes IV: " + oiv_value);
+      } else {
+        oiv_value = iv.getImpliedVolatility(
+          arguments[0],
+          arguments[1],
+          arguments[2],
+          arguments[3],
+          arguments[4],
+          arguments[5]
+        );
+        receivedMessage.channel.send("Implied Volatility: " + oiv_value);
+      }
+    }
+  } else {
+    receivedMessage.channel.send("Did not understand command.");
+  }
+}
+// utility functions
+function default_cmp(a, b) {
+  if (a == b) return 0;
+  return a < b ? -1 : 1;
+}
+
+function getCmpFunc(primer, reverse) {
+  var cmp = default_cmp;
+  if (primer) {
+    cmp = function(a, b) {
+      return default_cmp(primer(a), primer(b));
+    };
+  }
+  if (reverse) {
+    return function(a, b) {
+      return -1 * cmp(a, b);
+    };
+  }
+  return cmp;
+}
+
+function sort_by() {
+  var fields = [],
+    n_fields = arguments.length,
+    field,
+    name,
+    reverse,
+    cmp;
+
+  // preprocess sorting options
+  for (var i = 0; i < n_fields; i++) {
+    field = arguments[i];
+    if (typeof field === "string") {
+      name = field;
+      cmp = default_cmp;
+    } else {
+      name = field.name;
+      cmp = getCmpFunc(field.primer, field.reverse);
+    }
+    fields.push({
+      name: name,
+      cmp: cmp
+    });
+  }
+
+  return function(A, B) {
+    var a, b, name, cmp, result;
+    for (var i = 0, l = n_fields; i < l; i++) {
+      result = 0;
+      field = fields[i];
+      name = field.name;
+      cmp = field.cmp;
+
+      result = cmp(A[name], B[name]);
+      if (result !== 0) break;
+    }
+    return result;
+  };
+}
+
+discord_client.login(process.env.BOT_TOKEN);
